e259698c2882ac8cff1192b9399d8f9da8e53194
D123456e <159087933+D123456e@users.noreply.github.com>
2024-05-21 15:45:40 +0100

    Add files via upload

---
+++ cmp202/main.java
@@ -0,0 +1,16 @@
+Public class Main {
+    public static void main(string[]args){
+        CMP202 printUser = new CMP202();
+        printUser.printuserdetails();
+        Person student = new 
+        Person ("emmanuel", "egbuson","12345", "BHU/23/04/05/0097","123");
+        Person staff = new Person ("Staff Name", "Bingham", "234234", null,445);
+        System.out.printIn(staff);
+
+        //
+        System.out.printIn(person.getName())
+        //
+        system.out.printIn(person.setPassword("146132"));;
+        //person.password = "12345";
+    }
+}
